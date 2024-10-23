7cadf56daab4015ab46a2cf17a3a87e16ba88290
Tamara Najman <tamara.najman@crafters.cloud>
2024-10-23 15:15:23 +0200

    Added user list component and search functionality

--- src/App/angular-app/src/app/app.config.ts
+++ src/App/angular-app/src/app/app.config.ts
@@ -7,5 +7,9 @@ import { API_BASE_URL } from './api/api-reference';
 import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), {provide: API_BASE_URL, useValue: environment.apiUrl}]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: API_BASE_URL, useValue: environment.apiUrl }]
 };
