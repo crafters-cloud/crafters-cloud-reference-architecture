9b8bb22791f3addaa5d5ca26fc2811e1fb91d714
Tamara Najman <tamara.najman@crafters.cloud>
2024-10-30 17:44:23 +0100

    separated user details and user edit

--- src/App/angular-app/src/app/app.routes.ts
+++ src/App/angular-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './user/users-list/users-list.component';
 import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 
 export const routes: Routes = [
@@ -14,5 +15,10 @@ export const routes: Routes = [
         path: 'user/:id',
         component: UserDetailsComponent,
         title: 'User details'
+    },
+    {
+        path: 'user/:id/edit',
+        component: UserEditComponent,
+        title: 'Edit user'
     }
 ];
--- src/App/angular-app/src/app/user/user-details/user-details.component.ts
+++ src/App/angular-app/src/app/user/user-details/user-details.component.ts
@@ -1,56 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { CommonModule, Location } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
-import { CreateOrUpdateUserCommand, GetUserDetailsResponse, LookupResponseOfUserStatusId, UsersClient } from '../../api/api-reference';
+import { CommonModule } from '@angular/common';
+import { GetUserDetailsResponse, UsersClient } from '../../api/api-reference';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
 
 export class UserDetailsComponent implements OnInit{
   route: ActivatedRoute = inject(ActivatedRoute);
-  editMode: boolean = false;  
-
   user!: GetUserDetailsResponse;
-  statuses: LookupResponseOfUserStatusId[] = [];
-
-  editForm = new FormGroup({
-    fullName: new FormControl<string>(''),
-    emailAddress: new FormControl('', [Validators.email, Validators.required]),  
-    userStatus: new FormControl<LookupResponseOfUserStatusId | null>(null),
-  })
    
-  constructor(private usersClient: UsersClient, public location: Location){}
+  constructor(private usersClient: UsersClient){}
 
   ngOnInit(): void {    
-    const client = this.usersClient;
-
-    client.get(this.route.snapshot.params['id'])
-          .subscribe(user => this.user = user);    
-    
-    client.getStatusesLookup()
-          .subscribe(statuses => {this.statuses = statuses});
-  }
-
-  submitEditedUser() {  
-    const controls = this.editForm.controls;
-    
-    const command = new CreateOrUpdateUserCommand({
-      id: this.user.id,
-      emailAddress: controls.emailAddress.value!,
-      fullName: controls.fullName.value!,
-      roleId: this.user.roleId,
-      userStatusId: controls.userStatus.value?.value
-    });
-
-    this.usersClient.post(command)
-    .subscribe({
-      next: () => this.location.back()
-  });
-  }
+    this.usersClient.get(this.route.snapshot.params['id'])
+                    .subscribe(user => this.user = user);    
+  }  
 }
