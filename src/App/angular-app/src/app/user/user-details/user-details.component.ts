41eacaec6a622b0bd168c31c505954d50510759d
Tamara Najman <tamara.najman@crafters.cloud>
2024-10-30 14:36:39 +0100

    Edit user added

--- src/App/angular-app/src/app/user/user-details/user-details.component.ts
+++ src/App/angular-app/src/app/user/user-details/user-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CreateOrUpdateUserCommand, UsersClient } from '../../api/api-reference';
+import { CreateOrUpdateUserCommand, LookupResponseOfGuid, LookupResponseOfUserStatusId, UsersClient, UserStatusId } from '../../api/api-reference';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
@@ -16,43 +16,54 @@ export class UserDetailsComponent implements OnInit{
 
   route: ActivatedRoute = inject(ActivatedRoute);
   editMode: boolean = false;
+  isEmailAddressValid: boolean = true;
 
-  @Input() id: string = "";
+  id: string = "";
   emailAddress: string = "";
-  fullName: string = "";
+  fullName: string  = "";
   userStatusName: string = "";
   createdOn?: Date;
   updatedOn?: Date;
   userStatusDescription?: string;
-
-
+  userStatusId: UserStatusId | undefined;
+  userRoleId: string | undefined = "";
+  roles : LookupResponseOfGuid[] = [];
+  statuses: LookupResponseOfUserStatusId[] = [];
   editForm = new FormGroup({
     fullName: new FormControl(''),
     emailAddress: new FormControl(''),  
     userStatus: new FormControl(''),
   })
-
-
+   
   constructor(private usersClient: UsersClient){}
 
   ngOnInit(): void {    
+    this.id = this.route.snapshot.params['id'];
     this.usersClient.get(this.route.snapshot.params['id']).subscribe(user => {
       this.emailAddress = user.emailAddress ?? ""; 
       this.fullName = user.fullName ?? "";
       this.userStatusName = user.userStatusName ?? "";
-      this.userStatusDescription = user.userStatusDescription ?? "";
+      this.userStatusId = user.userStatusId;
+      this.userRoleId = user.roleId;
       this.createdOn = user.createdOn;
       this.updatedOn = user.updatedOn;
     })
+    
+    this.usersClient.getRolesLookup().subscribe(roles => {this.roles = roles;});
+    this.usersClient.getStatusesLookup().subscribe(statuses => {this.statuses = statuses});
   }
 
-  submitEditedUser() {
+  submitEditedUser() {  
+    this.userStatusId = this.statuses.find(s => s.label == this.editForm.controls.userStatus.value)?.value;
+
     const command = new CreateOrUpdateUserCommand({
-      id : this.id,
-      emailAddress: this.emailAddress,
-      fullName: this.fullName
+      id: this.id,
+      emailAddress: this.editForm.controls.emailAddress.value?.toString(),
+      fullName: this.editForm.controls.fullName.value?.toString(),
+      roleId: this.userRoleId,
+      userStatusId: this.userStatusId
     });
 
-    this.usersClient.post(command);
+    this.usersClient.post(command).subscribe(result => console.log(result));
   }
 }
