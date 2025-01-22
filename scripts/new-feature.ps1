f55b376373c00271d9e00cfdf826f24f62f00791
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-01-22 16:31:19 +0100

    Correct the connection string. Update new-feature.ps1

--- scripts/new-feature.ps1
+++ scripts/new-feature.ps1
@@ -22,14 +22,16 @@ else
     dotnet new crafters-feature --projectName "Client.Project" --featureName $featureName --featureNamePlural $featureNamePlural --allow-scripts yes
 
     Write-Host "New feature added successfully! To complete the process you need to:
-        1. Add permissions for the new feature: ``$featureName`` in the ``PermissionId.cs``
-        2. Update ``DatabaseSeeding.cs`` and add next two lines to the list of seedings:
-            ``new ${featureName}StatusSeeding(),
+        1. Add permissions for the read/write feature: ``$featureName`` in the ``PermissionId.cs``
+        2. Update ``MigrationSeeding.cs`` and seeding for the new feature:
+            ``new ${featureName}StatusSeeding()``
             new ${featureName}Seeding()``
-        3. Update ``TestDatabase.cs`` and the following line to the list of tables to ignore (this is need to avoid deleting the seeded data from the table between tests):
+        3. Update ``DbContextSeeding.cs`` and seeding for the new feature:
+            ``new ${featureName}Seeding()``
+        4. Update ``TestDatabase.cs`` and the following line to the list of tables to ignore (this is need to avoid deleting the seeded data from the table between tests):
             ``nameof(${featureName}Status)``
-        4. Add new database migration (run ``add-migration.ps1`` script)
-        5. Run the tests and adjust received Verify files to make sure everything is working as expected
-        6. Enjoy your new feature!
+        5. Add new database migration (run ``add-migration.ps1`` script)
+        6. Run the tests and adjust received Verify files to make sure everything is working as expected
+        7. Enjoy your new feature!
 "
 }
