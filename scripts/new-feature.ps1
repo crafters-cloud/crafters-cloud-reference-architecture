95a029ca989e02f69c50db17df3b7c386b486454
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-02-21 15:47:11 +0100

    Correct new-feature.ps1

--- scripts/new-feature.ps1
+++ scripts/new-feature.ps1
@@ -19,13 +19,13 @@ else
 {
     # Navigate to the root of the solution
     Set-Location ..
-    dotnet new crafters-feature --projectName "Client.Project" --featureName $featureName --featureNamePlural $featureNamePlural --allow-scripts yes
+    dotnet new crafters-feature --projectName "CraftersCloud.ReferenceArchitecture" --featureName $featureName --featureNamePlural $featureNamePlural --allow-scripts yes
 
     Write-Host "New feature added successfully! To complete the process you need to:
-        1. Add permissions for the read/write feature: ``$featureName`` in the ``PermissionId.cs``
+        1. Add permissions for the read/write feature: ``$featureName`` in the ``PermissionId.cs``:
+            ``${featureName}Read`` and ``${featureName}Write``
         2. Update ``MigrationSeeding.cs`` and seeding for the new feature:
             ``new ${featureName}StatusSeeding()``
-            new ${featureName}Seeding()``
         3. Update ``DbContextSeeding.cs`` and seeding for the new feature:
             ``new ${featureName}Seeding()``
         4. Update ``TestDatabase.cs`` and the following line to the list of tables to ignore (this is need to avoid deleting the seeded data from the table between tests):
