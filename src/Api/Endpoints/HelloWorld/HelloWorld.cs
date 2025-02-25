073d783e3fdac0230c621b31ca8a3921152c8728
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-02-25 15:18:08 +0100

    Add example of complex binding

--- src/Api/Endpoints/HelloWorld/HelloWorld.cs
+++ src/Api/Endpoints/HelloWorld/HelloWorld.cs
@@ -1,5 +1,4 @@
-﻿using CraftersCloud.ReferenceArchitecture.Api.MinimalApi;
-using CraftersCloud.ReferenceArchitecture.Application.Identity.GetUserById;
+﻿using System.Text.Json;
 using CraftersCloud.ReferenceArchitecture.Domain.Users;
 
 namespace CraftersCloud.ReferenceArchitecture.Api.Endpoints.HelloWorld;
@@ -7,16 +6,61 @@ namespace CraftersCloud.ReferenceArchitecture.Api.Endpoints.HelloWorld;
 public static class HelloWorld
 {
     [PublicAPI]
+    public class LineItem
+    {
+        public string ItemId { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+    }
+
+    [PublicAPI]
+    [UsedImplicitly]
+    public class LineItems(List<LineItem> items) : IParsable<LineItems>
+    {
+        public List<LineItem> Items { get; } = items;
+
+        public static LineItems Parse(string s, IFormatProvider? provider)
+        {
+            if (!TryParse(s, provider, out var request))
+            {
+                throw new FormatException("Cannot parse the provided input into a valid Request object.");
+            }
+
+            return request;
+        }
+
+        public static bool TryParse(string? s, IFormatProvider? provider, out LineItems result)
+        {
+            result = null!;
+
+            try
+            {
+                var items = s != null ? JsonSerializer.Deserialize<List<LineItem>>(s)! : [];
+                result = new LineItems(items);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+    [PublicAPI]
     public class Response
     {
-        public required string UserFullName { get; init; } = string.Empty;
+        public UserId UserId { get; set; }
+        public required int ItemsCount { get; init; }
+        public required int ItemsTotalQuantity { get; init; }
     };
-    
-    public static async Task<Results<Ok<Response>, NotFound>> Handle(UserId? id, ISender sender, ILogger<HelloWorldEndpoints> logger,
-        CancellationToken cancellationToken)
+
+    public static async Task<Ok<Response>> Handle([FromQuery] UserId userId, [FromQuery] LineItems lineItems,
+        ILogger<HelloWorldEndpoints> logger)
     {
-        var query = new Query(id ?? UserId.SystemUserId);
-        var response = await sender.Send(query, cancellationToken);
-        return response.ToMinimalApiResult(x => new Response { UserFullName = x.FirstName + x.LastName });
+        logger.LogInformation("Items count: {ItemsCount}", lineItems.Items.Count);
+        await Task.CompletedTask;
+        return TypedResults.Ok(new Response
+        {
+            UserId = userId, ItemsCount = lineItems.Items.Count, ItemsTotalQuantity = lineItems.Items.Sum(x => x.Quantity)
+        });
     }
 }
--- tests/Api.Tests/Endpoints/HelloWorldEndpointsFixture.cs
+++ tests/Api.Tests/Endpoints/HelloWorldEndpointsFixture.cs
@@ -11,8 +11,10 @@ public class HelloWorldEndpointsFixture : EndpointsFixtureBase
     public async Task HelloWorld()
     {
         var response = await Endpoint
+            .AppendQueryParam("userId", "A18C0A9D-A0AD-4D29-9187-B93A5A214AB8")
+            .AppendQueryParam("lineItems", """[{"ItemId":"1","Quantity":2},{"ItemId":"2","Quantity":3}]""")
             .GetJsonAsync<HelloWorld.Response>();
 
-        await Verify(response);
+        await Verify(response).DontScrubGuids();
     }
 }
