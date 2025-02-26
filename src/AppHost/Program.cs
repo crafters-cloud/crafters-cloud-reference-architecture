f2750ce067beefac4b23381c729a10268dffb28e
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-02-26 10:37:49 +0100

    Service bus

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Provisioning.ServiceBus;
 using CraftersCloud.ReferenceArchitecture.AppHost;
 using Projects;
 
@@ -28,5 +29,41 @@ builder.AddProject<Api>("api")
     .WithReference(cache)
     .WithReference(database)
     .WaitFor(migrations);
+
+var serviceBus = builder.AddAzureServiceBus("sbemulator");
+
+serviceBus.AddQueue("queue1")
+    .WithProperties(queue => queue.DeadLetteringOnMessageExpiration = false);
+
+serviceBus
+    .WithQueue("queue1", queue =>
+    {
+        queue.DeadLetteringOnMessageExpiration = false;
+    })
+    .WithTopic("topic1", topic =>
+    {
+        var subscription = new ServiceBusSubscription("sub1")
+        {
+            MaxDeliveryCount = 10,
+        };
+        topic.Subscriptions.Add(subscription);
+
+        var rule = new ServiceBusRule("app-prop-filter-1")
+        {
+            CorrelationFilter = new()
+            {
+                ContentType = "application/text",
+                CorrelationId = "id1",
+                Subject = "subject1",
+                MessageId = "msgid1",
+                ReplyTo = "someQueue",
+                ReplyToSessionId = "sessionId",
+                SessionId = "session1",
+                SendTo = "xyz"
+            }
+        };
+        subscription.Rules.Add(rule);
+    })
+    ;
 
 await builder.Build().RunAsync();
